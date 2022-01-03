304be4f32ff37f53e4aac85951b9a3e5aca8e146
Karl Du <18556906294@163.com>
2022-01-03 22:11:00 +0800

    add nunit

---
+++ Sixpence.ORM.Test/BaseEntityTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Sixpence.ORM.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sixpence.ORM.Test
+{
+    [TestFixture]
+    internal class BaseEntityTest
+    {
+        private account _account;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _account = new account() { code = "A001", name = "Test", Id = Guid.NewGuid().ToString() };
+        }
+
+        [Test]
+        public void ContainsKey()
+        {
+            Assert.IsTrue(_account.ContainKey("code"));
+        }
+    }
+
+    [Entity("account", "客户", false)]
+    class account : BaseEntity
+    {
+        public string code { get; set; }
+    }
+}
