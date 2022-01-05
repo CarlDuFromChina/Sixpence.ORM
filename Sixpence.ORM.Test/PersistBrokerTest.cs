9ad73daffec3fa2886a8b1b18e42ca8f31860142
CarlDuFromChina <18556906294@163.com>
2022-01-05 15:25:38 +0800

    update sixpence.common 1.0.1

---
+++ Sixpence.ORM.Test/PersistBrokerTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Sixpence.ORM.Broker;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sixpence.ORM.Test
+{
+    [TestFixture]
+    internal class PersistBrokerTest
+    {
+        IPersistBroker broker;
+
+        [SetUp]
+        public void SetUp()
+        {
+            broker = PersistBrokerFactory.GetPersistBroker();
+        }
+
+        [Test]
+        public void Check_Connection_IsOpen()
+        {
+            Assert.IsTrue(broker.DbClient.ConnectionState == System.Data.ConnectionState.Open);
+        }
+    }
+}
