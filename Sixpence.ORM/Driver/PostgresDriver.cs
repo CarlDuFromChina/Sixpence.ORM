4e636440c0e48cde6a6cf9d922a388f6e75377d5
Karl Du <18556906294@163.com>
2022-06-23 21:46:13 +0800

    update @sixpence.common

--- Sixpence.ORM/Driver/PostgresDriver.cs
+++ Sixpence.ORM/Driver/PostgresDriver.cs
@@ -126,24 +126,24 @@ WHERE rolname = '{name}'";
                         {
                             var dataType = dataTable.Columns[columnName].DataType;
 
-                            if (dataType == typeof(bool))
-                                writer.Write(ConvertUtil.ConToBoolean(dr[columnName]));
-                            else if (dataType == typeof(int))
+                            if (dataType == typeof(bool) || dataType == typeof(bool?))
+                                writer.Write(ConvertUtil.ConToBoolean(dr[columnName]), NpgsqlTypes.NpgsqlDbType.Boolean);
+                            else if (dataType == typeof(int) || dataType == typeof(int?))
                                 writer.Write(ConvertUtil.ConToInt(dr[columnName]), NpgsqlTypes.NpgsqlDbType.Integer);
-                            else if (dataType == typeof(long))
+                            else if (dataType == typeof(long) || dataType == typeof(long?))
                                 writer.Write(ConvertUtil.ConToInt(dr[columnName]), NpgsqlTypes.NpgsqlDbType.Bigint);
-                            else if (dataType == typeof(short))
+                            else if (dataType == typeof(short) || dataType == typeof(short?))
                                 writer.Write(ConvertUtil.ConToInt(dr[columnName]), NpgsqlTypes.NpgsqlDbType.Int2Vector);
-                            else if (dataType == typeof(decimal))
+                            else if (dataType == typeof(decimal) || dataType == typeof(decimal?))
                                 writer.Write(ConvertUtil.ConToDecimal(dr[columnName]), NpgsqlTypes.NpgsqlDbType.Numeric);
-                            else if (dataTable.Columns[columnName].DataType == typeof(bool))
-                                writer.Write(ConvertUtil.ConToDecimal(dr[columnName]), NpgsqlTypes.NpgsqlDbType.Boolean);
-                            else if (dataTable.Columns[columnName].DataType == typeof(DateTime))
+                            else if (dataType == typeof(DateTime) || dataType == typeof(DateTime?))
                                 writer.Write(ConvertUtil.ConToDateTime(dr[columnName]), NpgsqlTypes.NpgsqlDbType.Timestamp);
-                            else if (dataTable.Columns[columnName].DataType == typeof(JToken))
+                            else if (dataType == typeof(JToken))
                                 writer.Write(dr[columnName].ToString(), NpgsqlTypes.NpgsqlDbType.Jsonb);
-                            else
+                            else if (dataType == typeof(string))
                                 writer.Write(dr[columnName].ToString());
+                            else
+                                throw new NotSupportedException($"Postgres不支持{dataType.Name}类型");
                         }
                     }
                 }
